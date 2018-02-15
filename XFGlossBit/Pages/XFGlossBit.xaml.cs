fae90f895925f6dda6c1db15e8ec5dcecbfad6ea
June <junecho.ucb@gmail.com>
2018-02-15 09:44:06 -0800

    CleanUp

---
+++ XFGlossBit/Pages/XFGlossBit.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XFGlossBit.Pages
+{
+    public partial class XFGlossBit : ContentPage
+    {
+        public XFGlossBit()
+        {
+            InitializeComponent();
+        }
+    }
+}
